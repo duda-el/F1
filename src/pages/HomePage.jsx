d9720b7f3f8fa164985e550c2abce73b57b3422b
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-06-03 23:57:03 +0400

    banner finsihed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
-import Leclerc from "../assets/images/leclerc.jpg";
-import Leclerc2 from "../assets/images/leclerc2.jpg";
+import Leclerc from "../assets/images/leclerc2.jpg";
+import "./HomePage.css"
 
 export default function HomePage() {
   return (
     <div style={{ width: "100%", height: "100vh", background: "#ffff" }}>
       <Header />
-      <div>
-        <h1>THE CLOSEST YOU CAN GET TO FORMULA 1®</h1>
+      <div className="banner">
+        <h1 className="mainText">THE CLOSEST YOU CAN <br /> GET TO FORMULA 1®</h1>
         <img
           src={Leclerc}
           alt="Leclerc"
