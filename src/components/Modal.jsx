75611c61a5c958849a85b601b56fdbe55256db78
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-06-03 21:58:40 +0400

    sign in is created

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { XMarkIcon } from '@heroicons/react/24/outline';
-import axios from 'axios'; // Ensure axios is imported
+import React, { useState, useEffect } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import axios from "axios"; // Ensure axios is imported
 
 const SignInModal = ({ isOpen, onClose }) => {
   // Move hooks outside of conditional return
@@ -40,16 +40,14 @@ const SignInModal = ({ isOpen, onClose }) => {
         }
       );
 
-      console.log("Response:", response.data.user);
-
       if (response.data.status === "success") {
         if (!isSignUp) {
-          localStorage.setItem("user", JSON.stringify(response.data.user)); // Fix key to "user"
+          localStorage.setItem("user", JSON.stringify(response.data.user));
           setUser(response.data.user);
-          console.log(user)
+          onClose();
+          window.location.reload();
         }
         setModali(false);
-        onClose(); // Close modal on successful sign-in
       } else {
         console.error(response.data.message);
       }
@@ -69,9 +67,13 @@ const SignInModal = ({ isOpen, onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-60">
       <div className="bg-custom-black p-8 rounded-lg shadow-lg w-11/12 max-w-md mx-auto relative">
         <h2 className="text-2xl mb-6 text-red-600 font-bold">Sign In</h2>
-        <form onSubmit={handleSubmit}> {/* Add onSubmit */}
+        <form onSubmit={handleSubmit}>
+          {" "}
+          {/* Add onSubmit */}
           <div className="mb-6">
-            <label className="block text-sm font-medium text-gray-300">Email</label>
+            <label className="block text-sm font-medium text-gray-300">
+              Email
+            </label>
             <input
               type="email"
               className="mt-2 block w-full border border-gray-600 rounded py-2 px-4 bg-gray-800 text-gray-300 focus:outline-none focus:border-red-600"
@@ -82,7 +84,9 @@ const SignInModal = ({ isOpen, onClose }) => {
             />
           </div>
           <div className="mb-6">
-            <label className="block text-sm font-medium text-gray-300">Password</label>
+            <label className="block text-sm font-medium text-gray-300">
+              Password
+            </label>
             <input
               type="password"
               className="mt-2 block w-full border border-gray-600 rounded py-2 px-4 bg-gray-800 text-gray-300 focus:outline-none focus:border-red-600"
