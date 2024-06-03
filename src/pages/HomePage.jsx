851fadfcf0811aa1e60ab1e7d88756eeb4951d29
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-06-03 23:44:24 +0400

    added img

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
-import React from "react"
-import Header from "../components/Header"
+import React from "react";
+import Header from "../components/Header";
+import Leclerc from "../assets/images/leclerc.jpg";
+import Leclerc2 from "../assets/images/leclerc2.jpg";
 
-export default function HomePage(){
-    return(
-        <div style={{width: "100%", height: "100vh", background: "#ffff"}}>
-            <Header/>
-        </div>
-    )
+export default function HomePage() {
+  return (
+    <div style={{ width: "100%", height: "100vh", background: "#ffff" }}>
+      <Header />
+      <div>
+        <h1>THE CLOSEST YOU CAN GET TO FORMULA 1®</h1>
+        <img
+          src={Leclerc}
+          alt="Leclerc"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </div>
+    </div>
+  );
 }
