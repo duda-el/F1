98ebda3536bf14c8063287bd20bda8264abc1610
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-06-04 03:02:19 +0400

    finished cards

--- src/components/F1Schedule/F1ScheduleCard/F1ScheduleCard.jsx
+++ src/components/F1Schedule/F1ScheduleCard/F1ScheduleCard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./F1ScheduleCard.css";
 
-const F1ScheduleCard = (flag, title, location, date) => {
+const F1ScheduleCard = ({ flag, title, location, date }) => {
   return (
     <div className="card">
-      <h2 className="flag">Flag</h2>
-      <h3 className="title">FORMULA 1 AWS GRAND PRIX DU CANADA 2024</h3>
-      <h4 className="location">MONTRÉAL, CANADA</h4>
-      <h5 className="date">06-09 Jun</h5>
+      <h2 className="flag">{flag}</h2>
+      <h3 className="title">{title}</h3>
+      <h4 className="location">{location}</h4>
+      <h5 className="date">{date}</h5>
     </div>
   );
 };
+
 export default F1ScheduleCard;
